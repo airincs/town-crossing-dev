d2a4dcfdb6be396f56d36f09d2fda88e27a5433f
AirinCompSci <airincompsci@gmail.com>
2022-05-04 19:45:08 -0400

    Updated security

--- client/src/actions/notes.ts
+++ client/src/actions/notes.ts
@@ -1,10 +1,9 @@
-import { FETCH_ALL_NOTES, CREATE_NOTE, DELETE_NOTE, LOVE_NOTE } from '../constants/actionTypes';
 import * as api from '../api';
 
 export const getNotes = () => async (dispatch: any) => {
   try {
     const {data} = await api.fetchNotes();
-    dispatch({type: FETCH_ALL_NOTES, payload: data});
+    dispatch({type: 'FETCH_ALL_NOTES', payload: data});
   } catch (error: any) {
     console.log(error.message)
   } 
@@ -13,7 +12,7 @@ export const getNotes = () => async (dispatch: any) => {
 export const createNote = (note: any) => async (dispatch: any) => {
   try {
     const {data} = await api.createNote(note);
-    dispatch({type: CREATE_NOTE, payload: data})
+    dispatch({type: 'CREATE_NOTE', payload: data})
   } catch (error) {
     console.log(error);
   }
@@ -22,7 +21,7 @@ export const createNote = (note: any) => async (dispatch: any) => {
 export const deleteNote = (id: any) => async (dispatch: any) => {
   try {
     await api.deleteNote(id);
-    dispatch({type: DELETE_NOTE, payload: id})
+    dispatch({type: "DELETE_NOTE", payload: id})
   } catch (error) {
     console.log(error)
   }
@@ -31,7 +30,7 @@ export const deleteNote = (id: any) => async (dispatch: any) => {
 export const loveNote = (id: any) => async (dispatch: any) => {
   try{
     const { data } = await api.loveNote(id);
-    dispatch({type: LOVE_NOTE, payload: data})
+    dispatch({type: 'LOVE_NOTE', payload: data})
   } catch (error) {
     console.log(error)
   }
--- client/src/constants/actionTypes.ts
+++
@@ -1,4 +0,0 @@
-export const FETCH_ALL_NOTES = 'FETCH_ALL_NOTES';
-export const CREATE_NOTE = 'CREATE_NOTE';
-export const DELETE_NOTE = 'DELETE_NOTE';
-export const LOVE_NOTE = 'LOVE_NOTE';
