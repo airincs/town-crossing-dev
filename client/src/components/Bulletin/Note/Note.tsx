21b0d417a1ab57b0444a78b6fe6932fccde9b396
AirinCompSci <airincompsci@gmail.com>
2022-05-08 10:14:33 -0400

    Implemented error message if love a post while not logged in

--- client/src/components/Bulletin/Note/Note.tsx
+++ client/src/components/Bulletin/Note/Note.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { Box, Text, Button } from "@chakra-ui/react";
+import { Box, Text, Button, createStandaloneToast } from "@chakra-ui/react";
 import { deleteNote, loveNote } from "../../../actions/notes";
 import { useDispatch } from "react-redux";
 import moment from "moment";
@@ -12,15 +12,32 @@ const Note = ({ note }: AppProps) => {
   const dispatch = useDispatch<any>();
   const user = JSON.parse(localStorage.getItem("profile")!);
 
+  const NotLoggedInAlert = () => {
+    const toast = createStandaloneToast();
+    return toast({
+      title: "Can not like!",
+      description: "Must log in to like a post!",
+      status: "error",
+      duration: 3000,
+      isClosable: true,
+    });
+  };
+
   return (
     <Box w={"100px"} h={"100px"} marginBottom={"45px"}>
       <Text fontSize="xs">Title: {note.title} </Text>
       <Text fontSize="xs">Message: {note.message}</Text>
       <Text fontSize="xs">By: {note.username}</Text>
       <Text fontSize="xs">{moment(note.timeCreated).fromNow()}</Text>
-      <Button size={"xs"} onClick={() => dispatch(loveNote(note._id))}>
-        <Text fontSize="xs">Love: {note.loveCount.length}</Text>
-      </Button>
+      {!user?.result ? (
+        <Button size={"xs"} onClick={() => NotLoggedInAlert()}>
+          <Text fontSize="xs">Love: {note.loveCount.length}</Text>
+        </Button>
+      ) : (
+        <Button size={"xs"} onClick={() => dispatch(loveNote(note._id))}>
+          <Text fontSize="xs">Love: {note.loveCount.length}</Text>
+        </Button>
+      )}
       <Button
         size={"xs"}
         marginTop={"5px"}
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,17 @@
 import React, { FC } from "react";
+import { Flex } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+import Note from "../components/Bulletin/Note/Note";
 
 const Home: FC = () => {
-  return <div>home</div>;
+  const notes = useSelector((state: any) => state.notes);
+  const user = JSON.parse(localStorage.getItem("profile")!);
+
+  return (
+    <Flex direction={"column"} align={"center"} mt={"30px"}>
+      home
+    </Flex>
+  );
 };
 
 export default Home;
